9c84e8774314655e6c392e9f10e82cc29af39462
VanyaMate <buhu653@mail.ru>
2023-05-02 18:38:20 +0300

    добавлены новые сервисы, исключения, мидлвейры

--- server/service/token-service.js
+++ server/service/token-service.js
@@ -1,10 +1,11 @@
 const { sign } = require('jsonwebtoken');
 const tokenModel = require('../models/token-model');
+const jwt = require('jsonwebtoken');
 
 class TokenService {
     async generateTokens (payload) {
         const accessToken = sign(payload, process.env.JWT_ACCESS_SECRET, { expiresIn: '30m' }, null);
-        const refreshToken = sign(payload, process.env.JWT_ACCESS_SECRET, { expiresIn: '30d' }, null);
+        const refreshToken = sign(payload, process.env.JWT_REFRESH_SECRET, { expiresIn: '30d' }, null);
 
         return {
             accessToken,
@@ -22,6 +23,38 @@ class TokenService {
 
         const token = await tokenModel.create({ user: userId, refreshToken });
         return token;
+    }
+
+    async findRefreshToken (refreshToken) {
+        const tokenData = await tokenModel.findOne({ refreshToken });
+        return tokenData;
+    }
+
+    async removeToken (refreshToken) {
+        const tokenData = await tokenModel.deleteOne({ refreshToken });
+        return tokenData;
+    }
+
+    validateAccessToken (accessToken) {
+        try {
+            const userData = jwt.verify(accessToken, process.env.JWT_ACCESS_SECRET);
+            return userData;
+        }
+        catch (e) {
+            return null;
+        }
+    }
+
+    validateRefreshToken (refreshToken) {
+        try {
+            console.log(refreshToken);
+            const userData = jwt.verify(refreshToken, process.env.JWT_REFRESH_SECRET);
+            console.log(userData);
+            return userData;
+        }
+        catch (e) {
+            return null;
+        }
     }
 }
 
--- server/service/user-service.js
+++ server/service/user-service.js
@@ -4,13 +4,14 @@ const uuid = require('uuid');
 const mailService = require('./mail-service');
 const tokenService = require('./token-service');
 const UserDto = require("../dtos/user-dto");
+const ApiError = require('../exceptions/api-error');
 
 class UserService {
     async registration (email, password) {
         const candidate = await UserModel.findOne({ email });
 
         if (candidate) {
-            throw new Error('Пользователь уже существует');
+            throw ApiError.BadRequest('Пользователь с таким email уже существует');
         }
 
         const hashPassword = await bcrypt.hash(password, 3);
@@ -20,7 +21,7 @@ class UserService {
             password: hashPassword,
             activationLink
         });
-        await mailService.sendActivationMail(email, activationLink);
+        await mailService.sendActivationMail(email, `${process.env.API_URL}/api/activate/${activationLink}`);
         const userDto = new UserDto(user);
         const tokens = await tokenService.generateTokens({...userDto});
         await tokenService.saveToken(userDto.id, tokens.refreshToken);
@@ -29,6 +30,74 @@ class UserService {
             ...tokens,
             user: userDto,
         }
+    }
+
+    async login (email, password) {
+        const user = await UserModel.findOne({ email });
+
+        if (!user || !bcrypt.compare(password, user.password)) {
+            throw ApiError.BadRequest('Неправильно введен логин или пароль');
+        }
+
+        const userDto = new UserDto(user);
+        const tokens = await tokenService.generateTokens({...userDto});
+        await tokenService.saveToken(userDto.id, tokens.refreshToken);
+
+        return {
+            ...tokens,
+            user: userDto,
+        }
+    }
+
+    async logout (refreshToken) {
+        const token = await tokenService.removeToken(refreshToken);
+        return token;
+    }
+
+    async activate (activationLink) {
+        const user = await UserModel.findOne({ activationLink });
+
+        if (!user) {
+            throw ApiError.BadRequest('Неверная ссылка активации');
+        }
+
+        if (user && user.isActivated) {
+            throw ApiError.BadRequest('Пользователь уже активирован');
+        }
+
+        user.isActivated = true;
+        await user.save();
+    }
+
+    async refresh (refreshToken) {
+        if (!refreshToken) {
+            throw ApiError.UnauthorizedError();
+        }
+
+        const userData = tokenService.validateRefreshToken(refreshToken);
+        const tokenFromDb = await tokenService.findRefreshToken(refreshToken);
+
+        console.log(userData);
+        console.log(tokenFromDb);
+
+        if (!userData || !tokenFromDb) {
+            throw ApiError.UnauthorizedError();
+        }
+
+        const user = await UserModel.findById(userData.id);
+        const userDto = new UserDto(user);
+        const tokens = await tokenService.generateTokens({...userDto});
+        await tokenService.saveToken(userDto.id, tokens.refreshToken);
+
+        return {
+            ...tokens,
+            user: userDto,
+        }
+    }
+
+    async getAllUsers () {
+        const users = await UserModel.find();
+        return users;
     }
 }
 
